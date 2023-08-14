20fcf0388d9476e783fea4e6f3fe7e131c93ece8
zahidhasan13 <zahidhasanofficial13@gmail.com>
2023-08-14 16:34:08 +0600

    add display name

--- src/components/Authentication/Authentication.jsx
+++ src/components/Authentication/Authentication.jsx
@@ -2,114 +2,147 @@
 import { useRef, useState } from "react";
 import Login from "../Login/Login";
 import Register from "../Register/Register";
-import { createUserWithEmailAndPassword, getAuth, sendEmailVerification, sendPasswordResetEmail, signInWithEmailAndPassword } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  sendEmailVerification,
+  sendPasswordResetEmail,
+  signInWithEmailAndPassword,
+  updateProfile,
+} from "firebase/auth";
 import app from "../../firebase/firebase.config.js";
-
 
 const auth = getAuth(app);
 
 const Authentication = () => {
-    const [toggle, setToggle] = useState(false);
-    const [error, setError] = useState('');
-    const [success, setSuccess] = useState('');
-    const emailRef = useRef();
+  const [toggle, setToggle] = useState(false);
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
+  const emailRef = useRef();
 
-    const handleToggle = () => {
-        setToggle(!toggle);
-    };
+  const handleToggle = () => {
+    setToggle(!toggle);
+  };
 
-    // Register handlers
-    const handleRegister = (e) => {
-        e.preventDefault();
-        setError('');
-        setSuccess('');
-        const email = e.target.email.value;
-        const password = e.target.password.value;
-        // Validation
+  // Register handlers
+  const handleRegister = (e) => {
+    e.preventDefault();
+    setError("");
+    setSuccess("");
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    const name = e.target.name.value;
+    // Validation
 
-        if(!(/^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[a-zA-Z!#$%@&? "])[a-zA-Z0-9!#$%@&?]{8,20}$/.test(password))){
-            setError("At least one special character, one digit, one lowercase character, one uppercase character, Minimum 8 characters, Maximum 20 characters");
-            return;
-        }
-
-        createUserWithEmailAndPassword(auth, email, password)
-        .then(result =>{
-            const user = result.user;
-            setSuccess('Registration successful');
-            setError('');
-            e.target.reset();
-            emailVarification(user);
-        })
-        .catch(err => {
-            setError(err.message);
-            setSuccess('');
-        })
+    if (
+      !/^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[a-zA-Z!#$%@&? "])[a-zA-Z0-9!#$%@&?]{8,20}$/.test(
+        password
+      )
+    ) {
+      setError(
+        "At least one special character, one digit, one lowercase character, one uppercase character, Minimum 8 characters, Maximum 20 characters"
+      );
+      return;
     }
 
-    // Login handlers
-    const handleLogin = (e) => {
-        e.preventDefault();
-        setError('');
-        setSuccess('');
-        const email = e.target.email.value;
-        const password = e.target.password.value;
-        signInWithEmailAndPassword(auth, email, password)
-      .then(result =>{
-            const user = result.user;
-            setSuccess('Login successful')
-            setError('');
-            e.target.reset();
-        })
-      .catch(err => {
-        setError("Invalid username or password")
-        setSuccess('');
-        })
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+        setSuccess("Registration successful");
+        setError("");
+        e.target.reset();
+        emailVerification(user);
+        updateUserData(user, name);
+      })
+      .catch((err) => {
+        setError("Already use this email");
+        setSuccess("");
+      });
+  };
+
+  // Login handlers
+  const handleLogin = (e) => {
+    e.preventDefault();
+    setError("");
+    setSuccess("");
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    signInWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        const user = result.user;
+        setSuccess("Login successful");
+        setError("");
+        e.target.reset();
+      })
+      .catch((err) => {
+        setError("Invalid username or password");
+        setSuccess("");
+      });
+  };
+
+  // User Data
+  const updateUserData = (user, name) => {
+    updateProfile(user, {
+      displayName: name,
+    })
+      .then(() => {
+        console.log("user updated");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  //Email Varification
+  const emailVerification = (user) => {
+    sendEmailVerification(user).then(() => {
+      alert("Check your email");
+    });
+  };
+
+  // Reset Password
+  const handleResetPassword = (e) => {
+    const email = emailRef.current.value;
+    if (!email) {
+      alert("Please enter your email");
     }
+    sendPasswordResetEmail(auth, email)
+      .then(() => {
+        alert("Check your email");
+      })
+      .catch((err) => {
+        alert(err.message);
+      });
+  };
 
-    //Email Varification
-    const emailVarification = (user) => {
-        sendEmailVerification(user)
-        .then(() => {
-            alert("Check your email")
-        })
-    }
-
-    // Reset Password
-    const handleResetPassword = (e) => {
-        const email = emailRef.current.value;
-        if(!email) {
-            alert("Please enter your email");
-        }
-        sendPasswordResetEmail(auth, email)
-        .then(() => {
-            alert("Check your email");
-        })
-        .catch((err) => {
-            alert(err.message);
-        })
-    }
-
-
-    return (
-        <div className="text-center mt-10">
-            <button onClick={handleToggle} className="bg-blue-500 py-2 px-8 rounded text-white font-bold">
-                {
-                    toggle? "Register" : "Log In"
-                }
-            </button>
-            <div>
-                {
-                    toggle? <Login 
-                    handleLogin={handleLogin} 
-                    error={error} 
-                    success={success} 
-                    emailRef={emailRef} 
-                    handleResetPassword={handleResetPassword}/> 
-                    : <Register handleRegister={handleRegister} error={error} success={success}/>
-                }
-            </div>
-        </div>
-    );
+  return (
+    <div className="text-center mt-10">
+      <button
+        onClick={handleToggle}
+        className="bg-blue-500 py-2 px-8 rounded text-white font-bold"
+      >
+        {toggle ? "Register" : "Log In"}
+      </button>
+      <div>
+        {toggle ? (
+          <Login
+            handleLogin={handleLogin}
+            error={error}
+            success={success}
+            emailRef={emailRef}
+            handleResetPassword={handleResetPassword}
+          />
+        ) : (
+          <Register
+            handleRegister={handleRegister}
+            error={error}
+            success={success}
+          />
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Authentication;
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,6 +1,8 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react/no-unknown-property */
 
+import { useState } from "react";
+
 const Register = ({handleRegister, error, success}) => {
     const [showPassword, setShowPassword] = useState(false);
 
@@ -17,6 +19,10 @@ const Register = ({handleRegister, error, success}) => {
                   Create and account
               </h1>
               <form onSubmit={handleRegister} className="space-y-4 md:space-y-6">
+                  <div>
+                      <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your name</label>
+                      <input type="text" name="name" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Your name" required=""/>
+                  </div>
                   <div>
                       <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
                       <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required=""/>
